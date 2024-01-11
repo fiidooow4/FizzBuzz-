74442a0f0080612927efde39f121a19c9f0d8f32
ilyas Osman <ilyaso.fiiidooow@gmail.com>
2024-01-11 16:21:25 -0600

    new

---
+++ Complete the FizzBuzz Challenge/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+using System;
+
+#region FizzBuzz % 10
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }Console.ReadKey();
+    }
+}
+#endregion
+
+
+
+
+
+
+
+
+
+
